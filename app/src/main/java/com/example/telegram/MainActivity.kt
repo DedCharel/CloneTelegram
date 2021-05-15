276024d734dbb977c26c6749ec2538a345da9d4a
Daniil Shcherbakov <Charel@bk.ru>
2021-05-15 10:10:48 +0300

    Инициализация модели User

--- app/src/main/java/com/example/telegram/MainActivity.kt
+++ app/src/main/java/com/example/telegram/MainActivity.kt
@@ -6,14 +6,15 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import com.example.telegram.activities.RegisterActivity
 import com.example.telegram.databinding.ActivityMainBinding
+import com.example.telegram.models.User
 import com.example.telegram.ui.fragments.ChatsFragment
 import com.example.telegram.ui.objects.AppDrawer
-import com.example.telegram.utilits.AUTH
-import com.example.telegram.utilits.initFirebase
-import com.example.telegram.utilits.replaceActivity
-import com.example.telegram.utilits.replaceFragment
+import com.example.telegram.utilits.*
 import com.google.firebase.FirebaseApp
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ValueEventListener
 
 
 class MainActivity : AppCompatActivity() {
@@ -36,7 +37,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun initFunc() {
-        if (AUTH.currentUser!=null) {
+        if (AUTH.currentUser != null) {
             setSupportActionBar(mToolbar)
             mAppDrawer.create()
             replaceFragment(ChatsFragment(), false)
@@ -49,7 +50,14 @@ class MainActivity : AppCompatActivity() {
     private fun initFields() {
         mToolbar = mBinding.mainToolbar
         mAppDrawer = AppDrawer(this, mToolbar)
-
         initFirebase()
+        initUser()
+    }
+
+    private fun initUser() {
+        REF_DATABASE_ROOT.child(NODE_USERS).child(UID)
+            .addListenerForSingleValueEvent(AppValueEventListener{
+                USER = it.getValue(User::class.java) ?:User()
+            })
     }
 }
