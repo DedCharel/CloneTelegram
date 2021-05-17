ef0265f080909eb5ff1c3ba015ab599c28644ab0
Daniil Shcherbakov <Charel@bk.ru>
2021-05-17 21:36:44 +0300

    Рефакторинг и изменение bio

--- app/src/main/java/com/example/telegram/ui/fragments/SettingsFragment.kt
+++ app/src/main/java/com/example/telegram/ui/fragments/SettingsFragment.kt
@@ -26,8 +26,9 @@ class SettingsFragment : BaseFragment(R.layout.fragment_settings) {
         settings_full_name.text = USER.fullname
         settings_phone_number.text = USER.phone
         settings_status.text = USER.status
-        settings_username.text =USER.username
-        settings_btn_change_username.setOnClickListener { replaceFragment(ChangeUsernameFragment()) }
+        settings_username.text = USER.username
+        settings_btn_change_username.setOnClickListener {replaceFragment(ChangeUsernameFragment())}
+        settings_btn_change_bio.setOnClickListener { replaceFragment(ChangeBioFragment()) }
     }
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
@@ -35,7 +36,7 @@ class SettingsFragment : BaseFragment(R.layout.fragment_settings) {
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        when (item.itemId){
+        when (item.itemId) {
             R.id.settings_menu_exit -> {
                 AUTH.signOut()
                 (activity as MainActivity).replaceActivity(RegisterActivity())
