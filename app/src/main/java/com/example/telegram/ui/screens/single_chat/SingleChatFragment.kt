b0282b03f2b0d1e0d143cff25e07fb50f04326bc
Daniil Shcherbakov <Charel@bk.ru>
2021-06-15 15:36:05 +0300

    Отправка любых файлов в чат. BottomSheet.

--- app/src/main/java/com/example/telegram/ui/screens/single_chat/SingleChatFragment.kt
+++ app/src/main/java/com/example/telegram/ui/screens/single_chat/SingleChatFragment.kt
@@ -18,9 +18,11 @@ import com.example.telegram.models.UserModel
 import com.example.telegram.ui.screens.BaseFragment
 import com.example.telegram.ui.message_recycler_view.views.AppViewFactory
 import com.example.telegram.utilits.*
+import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.google.firebase.database.DatabaseReference
 import com.theartofdev.edmodo.cropper.CropImage
 import kotlinx.android.synthetic.main.activity_main.view.*
+import kotlinx.android.synthetic.main.choice_upload.*
 import kotlinx.android.synthetic.main.fragment_single_chat.*
 import kotlinx.android.synthetic.main.toolbar_info.view.*
 import kotlinx.coroutines.CoroutineScope
@@ -44,6 +46,7 @@ class SingleChatFragment(private val contact: CommonModel) : BaseFragment(R.layo
     private lateinit var mSwipeRefreshLayout: SwipeRefreshLayout
     private lateinit var mLayoutManager: LinearLayoutManager
     private lateinit var mAppVoiceRecorder: AppVoiceRecorder
+    private lateinit var mBottomSheetBehavior: BottomSheetBehavior<*>
 
 
     override fun onResume() {
@@ -55,6 +58,8 @@ class SingleChatFragment(private val contact: CommonModel) : BaseFragment(R.layo
 
     @SuppressLint("ClickableViewAccessibility")
     private fun initFields() {
+        mBottomSheetBehavior = BottomSheetBehavior.from(bottom_sheet_choice)
+        mBottomSheetBehavior.state = BottomSheetBehavior.STATE_HIDDEN
         mAppVoiceRecorder =  AppVoiceRecorder()
         mSwipeRefreshLayout = chat_swipe_refresh
         mLayoutManager = LinearLayoutManager(this.context)
@@ -70,20 +75,18 @@ class SingleChatFragment(private val contact: CommonModel) : BaseFragment(R.layo
                 chat_btn_voice.visibility = View.GONE
             }
         })
-        chat_btn_attach.setOnClickListener { attachFile() }
+        chat_btn_attach.setOnClickListener { attach() }
 
         CoroutineScope(Dispatchers.IO).launch {
 
             chat_btn_voice.setOnTouchListener { v, event ->
                 if(checkPermission(RECORD_AUDIO)){
                     if(event.action == MotionEvent.ACTION_DOWN){
-                        //TODO record
                         chat_input_message.setText("Запись")
                         chat_btn_voice.setColorFilter(ContextCompat.getColor(APP_ACTIVITY,R.color.primary))
                         val messageKey = getMessageKey(contact.id)
                         mAppVoiceRecorder.startRecord(messageKey)
                     }else if (event.action == MotionEvent.ACTION_UP){
-                        //TODO stop record
                         chat_input_message.setText("")
                         chat_btn_voice.colorFilter = null
                         mAppVoiceRecorder.stopRecord{file, messageKey ->
@@ -98,10 +101,19 @@ class SingleChatFragment(private val contact: CommonModel) : BaseFragment(R.layo
 
     }
 
+    private fun attach() {
+        mBottomSheetBehavior.state =BottomSheetBehavior.STATE_EXPANDED
+        btn_attach_file.setOnClickListener { attachFile() }
+        btn_attach_image.setOnClickListener { attachImage() }
+    }
 
+    private fun attachFile(){
+        val intent = Intent(Intent.ACTION_GET_CONTENT)
+        intent.type ="*/*"
+        startActivityForResult(intent, PICK_FILE_REQUEST_CODE)
+    }
 
-
-    private fun attachFile() {
+    private fun attachImage() {
         CropImage.activity()
             .setAspectRatio(1,1)
             .setRequestedSize(250,250)
@@ -199,14 +211,24 @@ class SingleChatFragment(private val contact: CommonModel) : BaseFragment(R.layo
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
-        if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE
-            && resultCode == Activity.RESULT_OK && data != null){
-            val uri = CropImage.getActivityResult(data).uri
-            val messageKey = getMessageKey(contact.id)
-            uploadFileToStorage(uri,messageKey,contact.id, TYPE_MESSAGE_IMAGE)
-            mSmoothScrollToPosition =true //опускаемся га последний элемент списка
-
+        if (data!=null) {
+            when (requestCode){
+                CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE -> {
+                    val uri = CropImage.getActivityResult(data).uri
+                    val messageKey = getMessageKey(contact.id)
+                    uploadFileToStorage(uri,messageKey,contact.id, TYPE_MESSAGE_IMAGE)
+                    mSmoothScrollToPosition =true //опускаемся га последний элемент списка
+                }
+                PICK_FILE_REQUEST_CODE -> {
+                    val uri = data.data
+                    val messageKey = getMessageKey(contact.id)
+                    uri?.let { uploadFileToStorage(it,messageKey,contact.id, TYPE_MESSAGE_FILE) }
+                    mSmoothScrollToPosition =true //опускаемся га последний элемент списка
+                }
+            }
         }
+
+
     }
 
 
