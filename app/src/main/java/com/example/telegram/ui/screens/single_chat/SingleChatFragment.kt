2c931d7b73fae9aff9cef00de75e21de7c8a7765
Daniil Shcherbakov <Charel@bk.ru>
2021-06-17 21:53:33 +0300

    Реализация главного экрана приложения.Сохрание пользователя по последнему сообщению в ноде main_list

--- app/src/main/java/com/example/telegram/ui/screens/single_chat/SingleChatFragment.kt
+++ app/src/main/java/com/example/telegram/ui/screens/single_chat/SingleChatFragment.kt
@@ -194,10 +194,13 @@ class SingleChatFragment(private val contact: CommonModel) : BaseFragment(R.layo
             if (message.isEmpty()) {
                 showToast("Введите текст сообщения")
             } else sendMessage(message, contact.id, TYPE_TEXT) {
+                saveToMainList(contact.id, TYPE_CHAT)
                 chat_input_message.setText("")
             }
         }
     }
+
+
 
 
     private fun initInfoToolbar() {
